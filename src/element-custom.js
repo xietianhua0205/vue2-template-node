64db427bf649ab78f0840ba723eb601c3a84efaa
jiangr <451183365@qq.com>
2021-10-08 23:17:55 +0800

    - fix: 还原forEach

--- src/element-custom.js
+++ src/element-custom.js
@@ -1,8 +1,8 @@
 import _ from 'lodash'
 import ElementUI from 'element-ui'
 
-window.APP_CONFIG.element.forEach((item, key) => {
-  item.forEach((it, prop) => {
+_.forEach(window.APP_CONFIG.element, (item, key) => {
+  _.forEach(item, (it, prop) => {
     ElementUI[_.upperFirst(key)].props[prop] = {
       default: item[prop]
     }
