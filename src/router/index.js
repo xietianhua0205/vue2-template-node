ea0c5099f9433a6c908b9d861e33d7964e0efb88
jiangr <451183365@qq.com>
2021-10-14 14:02:10 +0800

    - refactor: 退出登录优化

--- src/router/index.js
+++ src/router/index.js
@@ -48,11 +48,19 @@ const router = new Router({
 router.beforeEach((to, from, next) => {
   if (to.name === 'login') {
     clearUser()
-    if (window.APP_CONFIG.isDev) {
+    if (window.APP_CONFIG.isDev || window.APP_CONFIG.ENV === 'development') {
       next() // 本项目登录页面
-    } else { // 产品登录页面
+    } else { // 产品登录页面，以下3种选择一种，第三种无需服务器端额外配置，可以避免服务器配置问题引起的无限刷新当前页面问题，但登陆地址可能会有不同导致跳转失败
+      // 1、产品退出，302到登录页
+      const form = document.createElement('form')
+      form.action = '/logout'
+      form.method = 'POST'
+      document.body.appendChild(form)
+      form.submit()
+      // 2、刷新，302到登录页
       // top.location.reload() // 配置gateway后有效
-      top.location.replace('/sso/login') // 未配置gateway时有效
+      // 3、直接定位到登录页
+      // top.location.replace('/sso/login') // 未配置gateway时有效
     }
   } else {
     if (isNavAllow({ id: to.name })) {
