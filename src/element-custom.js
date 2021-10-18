28640c75079ffa5fbe36cb8aea6b5ff7163773f4
jiangr <451183365@qq.com>
2021-10-18 18:11:03 +0800

    - fix: 修复element配置覆盖后丢失部分默认配置的BUG

--- src/element-custom.js
+++ src/element-custom.js
@@ -2,9 +2,13 @@ import _ from 'lodash'
 import ElementUI from 'element-ui'
 
 _.forEach(window.APP_CONFIG.element, (item, key) => {
+  const props = ElementUI[_.upperFirst(key)].props
   _.forEach(item, (it, prop) => {
-    ElementUI[_.upperFirst(key)].props[prop] = {
-      default: item[prop]
+    if (typeof props[prop] === 'function' || (props[prop] instanceof Array)) {
+      props[prop] = {
+        type: props[prop]
+      }
     }
+    props[prop] = _.merge(props[prop], { default: item[prop] })
   })
 })
