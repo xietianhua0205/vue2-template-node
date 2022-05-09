d35bcd8638da96a64a3b2e93971db25e0c5c719d
lix <lixin@hiekn.com>
2022-05-09 17:53:19 +0800

    - fix: query-page.js number处理优化

--- src/mixins/query-page.js
+++ src/mixins/query-page.js
@@ -18,13 +18,20 @@ export default {
           const type = isObject ? settings[k].type || typeof settings[k].value : typeof settings[k]
           // eslint-disable-next-line valid-typeof
           let rv = (typeof v === 'string' || typeof v === type) ? v : isObject ? settings[k].value : settings[k]
-          switch (type) {
-            case 'number':
-              rv = parseInt(rv)
-              break
-            case 'json':
-              rv = rv && JSON.parse(rv)
-              break
+          if (isObject && settings[k].transform) {
+            rv = settings[k].transform(rv)
+          } else {
+            switch (type) {
+              case 'number':
+                rv = parseInt(rv)
+                if (isNaN(rv)) {
+                  rv = isObject ? settings[k].value : settings[k]
+                }
+                break
+              case 'json':
+                rv = rv && JSON.parse(rv)
+                break
+            }
           }
           this.$set(this.query, k, rv)
         })
@@ -49,7 +56,7 @@ export default {
           if (settings[k] instanceof Object) {
             key = settings[k].map || k
           }
-          if (this.query[k]) {
+          if (this.query[k] || this.query[k] === 0) {
             query[key] = this.query[k] instanceof Object ? JSON.stringify(this.query[k]) : this.query[k].toString()
           } else {
             delete query[key]
